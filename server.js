2628092ccd266a605093a639ac80363f92ce1ef8
iamfarooqi <you@example.com>
2022-01-14 20:53:59 +0500

    credential 1

--- server.js
+++ server.js
@@ -97,13 +97,13 @@ app.use(
 );
 
 
-// app.use(function (req, res, next) {
-//    //Enabling CORS
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//      next();
-//    });
+app.use(function (req, res, next) {
+   //Enabling CORS
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+     next();
+   });
 
 
 
